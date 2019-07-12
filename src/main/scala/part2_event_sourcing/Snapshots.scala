7ebe07ce6875f6174d58e4db86104fff6b37d1ac
rodrigo-pattusi <50590@sistemas.frc.utn.edu.ar>
2019-07-12 17:56:09 -0300

    11

--- src/main/scala/part2_event_sourcing/Snapshots.scala
+++ src/main/scala/part2_event_sourcing/Snapshots.scala
@@ -3,18 +3,27 @@ package part2_event_sourcing
 import akka.actor.{ActorLogging, ActorSystem, Props}
 import akka.persistence.PersistentActor
 
+import scala.collection.mutable
+
 object Snapshots extends App{
 
   // COMMANDS
   case class ReceiveMessage(contents: String) // message FROM your contact
-  case class SentMessa
+  case class SentMessage(contents: String) // message TO your contact
 
   // EVENTS
+  case class ReceivedMessageRecord(id: Int, contents: String)
+  case class SentMessageRecord(id: Int, contents: String)
 
   object Chat{
     def props(owner: String, contact: String) = Props(new Chat(owner, contact))
   }
   class Chat(owner: String, contact: String) extends PersistentActor with ActorLogging{
+
+    val MAX_MESSAGES = 10
+
+    var currentMessageId = 0
+    val lastMessages = new mutable.Queue[]()
 
     override def persistenceId: String = s"$owner-$contact-chat"
 
@@ -25,5 +34,6 @@ object Snapshots extends App{
 
   val system = ActorSystem("SnapshotDemo")
   val chat = system.actorOf(Chat.props("daniel123", "martin345"))
+
 
 }
