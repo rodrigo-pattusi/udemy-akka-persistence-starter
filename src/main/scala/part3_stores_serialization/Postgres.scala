7220c8464003e62ae2655509acbd919a82bf62b3
Rodrigo P <50590@sistemas.frc.utn.edu.ar>
2019-07-13 13:11:51 -0300

    14

--- src/main/scala/part3_stores_serialization/Postgres.scala
+++ src/main/scala/part3_stores_serialization/Postgres.scala
@@ -1,6 +1,21 @@
 package part3_stores_serialization
 
+import akka.actor.{ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
+
 object Postgres extends App {
 
+  val postgresActorSystem = ActorSystem("postgresSystem", ConfigFactory.load().getConfig("postgresDemo"))
+  val persistentActor = postgresActorSystem.actorOf(Props[SimplePersistentActor], "simplePersistenActor")
+
+  for (i <- 1 to 10) {
+    persistentActor ! s"I love Akka [$i]"
+  }
+  persistentActor ! "print"
+  persistentActor ! "snap"
+
+  for (i <- 11 to 20) {
+    persistentActor ! s"I love Akka [$i]"
+  }
 
 }
